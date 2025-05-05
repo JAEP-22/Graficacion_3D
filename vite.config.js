6e0bb0c3c3f7ab5123e8b3600c25bf50e0cf68b8
Jaep <jaep0928@gmail.com>
2025-05-05 15:37:14 -0600

    version 2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 export default {
-    // Set the base directory for GitHub pages
-    base: '/simcity-threejs-clone/',
+  // Set the base directory for GitHub pages
+  base: '/simcity-threejs-clone/',
+
+  // Set the project root directory (relative to the config file)
+  root: './src',
+
+  // Set the directory to serve static files from (relative to the root)
+  publicDir: './public',
   
-    // Set the project root directory (relative to the config file)
-    root: './src',
-  
-    // Set the directory to serve static files from (relative to the root)
-    publicDir: './public',
-    
-    // Set the build output directory
-    build: {
-      outDir: './dist'
-    }
+  // Set the build output directory
+  build: {
+    outDir: './dist'
   }
+}
